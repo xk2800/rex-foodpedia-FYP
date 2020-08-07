a60cc6cc29126ef0eee0f4e6a005761b09de44a0
Xavier K <59243836+xk2800@users.noreply.github.com>
2020-08-07 14:38:28 +0800

    gmail update

--- setup info files/Email_phpmailer/email_via_phpmailer(reset_password).php
+++ setup info files/Email_phpmailer/email_via_phpmailer(reset_password).php
@@ -34,8 +34,8 @@ if(isset($_POST["submit"])){
         $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                      // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'anythingwetest@gmail.com';                     // SMTP username
-        $mail->Password   = 'szzwveyaacuerjcx';                              // SMTP password
+        $mail->Username   = 'noreply.rexfoodipedia@gmail.com';                     // SMTP username
+        $mail->Password   = 'srzbcsrqcghavujd';                              // SMTP password
         $mail->SMTPSecure = "tls";         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
