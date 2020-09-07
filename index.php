6e65f4ce96ad695c438a17123adb70bc77d8252b
Restyled.io <commits@restyled.io>
2020-09-07 03:31:27 +0000

    Restyled by whitespace

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
     <!--Bootstrap CDN-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    
+
     <!--Font Awesome CDN-->
     <script src="https://kit.fontawesome.com/daa253e478.js" crossorigin="anonymous"></script>
 
@@ -50,10 +50,10 @@
 <body>
 
 <div id="all">
-    
+
 <!--LEVEL 1-->
 <!--TOP nav bar start-->
-    
+
     <nav class="navbar navbar-expand-sm navi">
         <!--LOGO-->
         <a class="navbar-brand" href="index"> <span class="logo" id="logo"><img src="img/logo/logo.jpg"></span></a>
@@ -79,7 +79,7 @@
     <span class="main"><img src="img/test/food1.jpg"></span>
     <span class="over-img-txt">
         <span class="hungry">Hungry?</span>
-        
+
     <!--TYPING EFFECT-->
     <div class="typewriter">
         <div id="typewriter"></div>
@@ -144,10 +144,10 @@
 <!--LEVEL 5 END-->
 </div>
 
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>   
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    
+
     <!--Scroll Effect CDN https://michalsnik.github.io/aos/-->
     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
     <script>
