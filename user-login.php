b783cdb0b53b0bfc3d8171832e15a31fe9f78a62
eltonwcm00 <eltonwcm00@gmail.com>
2020-12-03 22:19:20 +0800

    userlogin navbar fixed, homepage navbar issue

---
+++ user-login.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            
+            <title>Login | REX Foodipedia</title>
+
+            <!--FONTS.CSS-->
+            <link rel="preload" href="css/fonts.css" as="style">
+            <link rel="stylesheet" type="text/css" href="css/user-login.css">
+
+            <!--THIS IS FONT AWESOME JAVASCRIPT START-->
+            <script src="https://kit.fontawesome.com/daa253e478.js" crossorigin="anonymous"></script>
+            <!--THIS IS FONT AWESOME JAVASCRIPT END-->
+            <!--THIS IS BOOTSTRAP CSS PART START-->
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+            <!--THIS IS BOOTSTRAP CSS PART END-->
+            
+        </head>
+<!--<style>
+    #overlay{
+    background-color: #e8ded2;
+}
+</style>-->
+        <body>
+
+<!--div id="overlay">
+            <?php 
+                //include ("navbar.html")
+            ?> 
+            <div class = "container">
+                fhghghhjgfnhjhjghjghjghj
+                <!--div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                      <h5 class="card-title">Special title treatment</h5>
+                      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div-->
+            <!--/div-->
+            <!--div class = "bootstrap-setup"-->
+
+<div id="overlay">
+<!--LEVEL 1-->
+<?php
+    include ("navbar.html")
+?>
+<!--LEVEL 1 END-->
+
+            <div class = "container">
+
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                      <h5 class="card-title">Special title treatment</h5>
+                      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+            </div>
+<!--LEVEL 2-->
+
+    
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>   
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    
+</div>
+    </body>
+</html>
---
+++ xavier-test/test-1.php
@@ -0,0 +1,83 @@
+<?php
+    session_start();
+    include("db-connect.php");
+
+    /**
+     * using if else statement for homepage
+     */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="icon" type="image/png" href= "img/logo/logo.png">
+
+    <!--title>Home | REX Foodipedia</title-->
+
+<?php
+
+    //$_SESSION['logged-in'];
+
+    /**
+     * <p class="menu"><a href="index.html">Home</a></p> 
+     *    <p class="menu"><a href="Product1.html">Products</a> </p> 
+     * 
+     *  <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true): ?>
+     *   
+     * <p class="menu"><a href="logout.php">Logout</a></p>
+     * 
+     *   <?php else: ?>
+     * 
+     *   <p class="menu"><a href="login.html">Login</a></p> 
+     * 
+     *   <?php endif; ?>
+     * 
+     * 
+     * <?php if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true){?>
+     *      <title> Welcome <?php echo $_SESSION['uname']?> | REX Foodpedia</title>
+     * <?php } else { ?>
+     *      <title>Home | REX Foodipedia</title>
+     * <?php } ?>
+     * 
+     */
+    
+?>
+<?php if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true){?>
+                <title> Welcome <?php echo $_SESSION['uname']?> | REX Foodpedia</title>
+         <?php } else { ?>
+                <title>Home | REX Foodipedia</title>
+         <?php } ?>
+
+    
+    <!--Bootstrap CDN-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    
+    
+
+</head>
+<body></body>
+    
+
+<div id="overlay">
+<!--LEVEL 1-->
+<?php
+    include ("navbar.html")
+?>
+<!--LEVEL 1 END-->
+
+<!--LEVEL 2-->
+
+    
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>   
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    
+
+
+
+</body>
+</html>
