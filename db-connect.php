432f2c2c2712cb1921874b826cd696e0ee280d23
Xavier K <xavierkhew00@gmail.com>
2020-12-09 18:10:42 +0800

    change db IP

--- db-connect.php
+++ db-connect.php
@@ -1,13 +1,13 @@
 <?php
 
 //used for procedule php call
-$connect = mysqli_connect("18.141.239.138", "database", "DZUUuox3oDVC3s3A", "rex-foodipedia");
+$connect = mysqli_connect("13.212.90.225.239.138", "database", "DZUUuox3oDVC3s3A", "rex-foodipedia");
 
 //using PDO call
-$mysqli = NEW MySQLi ('18.141.239.138', 'database', 'DZUUuox3oDVC3s3A', 'rex-foodipedia');
+$mysqli = NEW MySQLi ('13.212.90.225.239.138', 'database', 'DZUUuox3oDVC3s3A', 'rex-foodipedia');
 
 //actual db connection
-@mysqli_connect("18.141.239.138", "database", "DZUUuox3oDVC3s3A", "rex-foodipedia") || die('<p>Error: 500. <br> DB not connected</p>');
+@mysqli_connect("13.212.90.225.239.138", "database", "DZUUuox3oDVC3s3A", "rex-foodipedia") || die('<p>Error: 500. <br> DB not connected</p>');
 
 ?>
 
