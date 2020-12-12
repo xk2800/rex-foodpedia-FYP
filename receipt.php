d665848c6363710f11c041ce152f6f4705487d6d
Xavier K <xavierkhew00@gmail.com>
2020-12-13 05:14:50 +0800

    make 50% layout of receipt

--- receipt.php
+++ receipt.php
@@ -9,16 +9,13 @@
 
 ?>
 <html>
-    <head><title>Welcome | REX Foodpedia</title>
+    <head><title>Your Receipt | REX Foodpedia</title>
 
     <meta name="viewpoint" content="width=device-width, initial-scale=1">
     <meta name="viewpoint" content="width=device-width">
     
 <!--INCLUDE START HERE-->
     <link rel="icon" type="image/png" href= "image/MYRUN 1.png">
-
-<!--FONTS.CSS-->
-    <link rel="stylesheet" href="css/fonts.css">
 
 <!--THIS IS FONT AWESOME JAVASCRIPT START-->
     <script src="https://kit.fontawesome.com/daa253e478.js" crossorigin="anonymous"></script>
@@ -28,13 +25,29 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 <!--THIS IS BOOTSTRAP CSS PART END-->
 
+<!--FONTS.CSS-->
+<link rel="stylesheet" href="css/fonts.css">
+
 </head>
 <body>
 
+    <div class="container">
+
+    <div class="paper">
+        <img src="img/logo/logo.png" alt="REX Foodipedia Logo" id="logo">
+        <p id="receipt title">***RECEIPT***</p>
+        <p id="receipt_id">Receipt ...</p>
+        <p id="date">...</p>
+        <p id="hori-line"><hr></p>
+        <p id="company-info">...</p>
+        <p></p>
+
+    </div>
 
 
+    </div>
 
-</div>
+
 <!--THIS IS BOOTSTRAP JAVASRIPT PART START-->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>   
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
