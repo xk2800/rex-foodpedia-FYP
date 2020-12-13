c189948bd90f7f0938353c46187c7316a708a87f
Xavier K <xavierkhew00@gmail.com>
2020-12-13 19:45:08 +0800

    receipt html and php basics done

--- receipt.php
+++ receipt.php
@@ -1,11 +1,7 @@
 <!DOCTYPE html>
 <?php
-/**
- * TODO: CSS FIX ***
- * TODO: FOOTER FIX ***
- * 
- *  * THIS IS USING BETTER COMMENTS *
- */
+    include "db-connect.php";
+    session_start();
 
 ?>
 <html>
@@ -35,13 +31,93 @@
 
     <div class="paper">
         <img src="img/logo/logo.png" alt="REX Foodipedia Logo" id="logo">
-        <p id="receipt title">***RECEIPT***</p>
-        <p id="receipt_id">Receipt ...</p>
-        <p id="date">...</p>
+        <p id="receipt title">RECEIPT</p>
+        <p id="send-type">....</p>
+        <p id="receipt_id">Receipt Number: ...</p>
+        <p id="date">Date: </p>
         <p id="hori-line"><hr></p>
         <p id="company-info">...</p>
-        <p></p>
+        <p id="cust-address">Delivery Address: ...</p>
 
+        <?php
+        //$query = "SELECT * FROM order_rec WHERE trans_id";
+        $result = mysqli_query($connect, $query);
+        if(mysqli_num_rows($result3)>0){
+        ?>
+        <span id="label">
+            <table>
+                <th id="dish-label">Dish</th>
+                <th id="quantity">Quantity</th>
+                <th id="unit-price">Unit Price</th>
+                <th id="price">Price</th>
+            </table>
+        </span>
+        <?php
+            while ($row3 = mysqli_fetch_assoc($result)) {
+            ?>
+                <span id="db-items">
+        <?php
+                echo '<table>';
+                echo '<tr>';
+                echo '<td>'.$row3['name'].'</td>';
+                echo '<td>'.$row3['email'].'</td>';
+                echo '<td style="width: 130px;">';
+                if($row3['vetting'] == "0"){
+                    echo "No submission";
+                }
+                else if($row3['vetting'] == "1")
+                {
+                    echo "Pending";
+                }else if($row3['vetting'] == "2"){
+                    echo "Reject";
+                } else if($row3['vetting'] == "3"){
+                    echo "Approved";
+                }else if($row3['vetting'] == "20"){
+                    echo "Reject Without Email Send";
+                }
+                '</td>';
+                echo '<td style="width: 130px; text-align: center;">'?><a href="resume?email=<?php echo $row3["email"];?>"><button type="submit">Update Status</button></a></td><?php
+                echo '</tr>';
+                echo '</table>';
+            }
+        ?>
+                </span>
+        <?php
+        }
+        ?>
+        <p id="hori-line"><hr></p>
+
+        <span class="payment-breakdown">
+            <table id="default-text">
+                <tr>
+                    <th id="subtotal">Subtotal</th>
+                    <th id="db-subtotal">RM SUBTOTAL</th>
+                </tr>
+                <tr>
+                    <td id="discount">- Discount</td>
+                    <td id="db-discount">RM DISCOUNT</td>
+                </tr>
+                <tr>
+                    <td id="voucher">- Voucher</td>
+                    <td id="db-voucher">RM VOUCHER</td>
+                </tr>
+                <tr>
+                    <p id="hori-line"><hr></p>
+                </tr>
+                <tr>
+                    <td id="tax">+Service Tax(6%)</td>
+                    <td id="db-tax">RM SERVICE TAX</td>
+                </tr>
+                <tr>
+                    <th id="total">Total</th>
+                    <th id="db-total">RM TOTAL</th>
+                </tr>
+                <tr>
+                    <th id="payment-type">Payment method: PAYMENT METHOD</th>
+                </tr>
+            </table>
+
+        </span>
     </div>
 
 
