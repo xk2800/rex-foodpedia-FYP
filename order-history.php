a40bf540b6d40517bb41f0bec705088bebd3a0b5
Xavier K <xavierkhew00@gmail.com>
2021-01-16 05:28:09 +0800

    ADD: group order history by trans if

--- order-history.php
+++ order-history.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <?php
-
+    include "db-connect.php";
+    session_start();
 ?>
 <html>
     <head>
@@ -46,6 +47,75 @@
 
                 <div id="order-list-item">
                     
+                    <?php
+                                        $order_history = mysqli_query($connect, "SELECT * from order_rec WHERE email='xavierkhew00@gmail.com' GROUP BY trans_id"); //where email='$email'
+
+                                        while($history_output = mysqli_fetch_assoc($order_history)){
+                        
+                                        $delivery_type = $history_output ["delivery_type"];
+                                        
+                                        //0 = self pick-up      //1 = delivery
+                                        if($delivery_type == "0"){
+                                            echo "<br>";
+                                            echo "Delivery by REX Foodipedia";
+                                            echo "<br>";
+
+                                        } else if($delivery_type == "1"){
+                                            echo "<br>";
+                                            echo "Pick up by customer";
+                                            echo "<br>";
+                                        }
+                                        
+                                        echo $history_output ["trans_id"];
+
+                                        $test = $history_output ["trans_id"];
+                    ?>
+                                        <!-- Food Amount & Name-->
+                    <?php
+
+                                            $chicken_fried_rice = mysqli_query($connect, "SELECT * from order_rec WHERE email='xavierkhew00@gmail.com' AND trans_id = '$test'");
+                                            while($qqq = mysqli_fetch_assoc($chicken_fried_rice)){
+
+
+                                                $dish_qty = $qqq ["dish_qty"];
+                                                $dish_name = $qqq ["dish_name"];
+
+
+
+                                                // Declare an array and initialize it 
+                                                $Array = array(" $dish_qty", ""  ); 
+                                                
+                                                // Display the array elements 
+                                                //echo($Array); 
+                                                
+                                                // Use implode() function to join 
+                                                // comma in the array 
+                                                $List = implode('x ', $Array); 
+                                                
+                                                // Display the comma separated list 
+                                                echo ($List); 
+
+                                                //echo $dish_name;
+
+
+                                                // Declare an array and initialize it 
+                                                $Array2 = array(" $dish_name", "" ); 
+                                                
+                                                // Display the array elements 
+                                                //echo ($Array2); 
+                                                
+                                                // Use implode() function to join 
+                                                // comma in the array 
+                                                $List2 = implode(', ', $Array2); 
+                                                
+                                                // Display the comma separated list 
+                                                echo ($List2); 
+                                                
+
+
+                                            }
+                                        }
+                    ?>
                 </div>
         </div>
     </section>
