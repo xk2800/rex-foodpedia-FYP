3f23719c33efb8c7eaf657427f833480ae8b62a8
eltonwcm00 <eltonwcm00@gmail.com>
2021-01-17 22:14:29 +0800

    cus status (1/3)

---
+++ admin/cus-status.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Account Statuses | REX Foodipedia</title>
+
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+            <!--INCLUDE START HERE-->
+            <link rel="icon" type="image/png" href= "img/logo/logo.png">
+
+            <!--THIS IS FONT AWESOME JAVASCRIPT START-->
+            <script src="https://kit.fontawesome.com/daa253e478.js" crossorigin="anonymous"></script>
+
+            <!--THIS IS BOOTSTRAP CSS PART START-->
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+            <!--FONTS.CSS-->
+            <link rel="stylesheet" href="css/fonts.css">
+
+        </head>
+        
+        <body style="background-color: #4C5C68;">
+
+            <?php
+                include("navbar.html");
+            ?>
+
+            <div class="container">
+                
+                <div class="row">
+                    <h4>Account Statuses</h4>  
+                    <button type="button" class="btn btn-primary btn-sm">+ Account Statuses</button>
+                </div> 
+                
+                <hr style="background-color:white"/>
+            
+                <!--ref : https://getbootstrap.com/docs/4.0/components/pagination/-->
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination">
+                        <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                    </ul>
+                </nav>
+            
+                <table class="table table-bordered" style="background-color:white;">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">3</th>
+                        <td>Larry the Bird</td>
+                        <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <!--THIS IS BOOTSTRAP JAVASRIPT PART START-->
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>   
+            <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        </body>
+</html>
