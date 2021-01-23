6cfe7be38d22886eee1c56b6fe6f6fa022f22e78
eltonwcm00 <eltonwcm00@gmail.com>
2021-01-24 00:28:25 +0800

    cus points add (1/3)

--- admin/cus-points-add.php
+++ admin/cus-points-add.php
@@ -16,12 +16,40 @@
 
         <!--FONTS.CSS-->
         <link rel="stylesheet" href="css/fonts.css">
-
     </head>
-    <body>
+    
+    <body style="background-color: #4C5C68;">     
+        
+        <?php
+            include("navbar.html");
+        ?>
+
+        <div class="container">
+            <form>
+                <div class="row mb-4 mt-5">
+                    <div class="form-group col-md-9">
+                        <input id="exampleFormControlInput5" type="email" placeholder="Insert the user email" class="form-control form-control-underlined">
+                    </div>
+                    <div class="form-group col-md-3">
+                        <button type="submit" class="btn btn-primary rounded-pill btn-block shadow-sm">Search</button>
+                    </div>
+                </div> 
+            </form>
+
+            <!-- if-else statement will go here, state=true -> statement block will be executed || state=false -> alert box display the error -->
+                <div class="card">
+                    <h5 class="card-header">Add Points</h5>
+                    <div class="card-body">
+                        <a href="#" class="btn btn-primary btn-sm float-right">Save</a>
+                    </div>    
+                </div>
+            <!-- end of if-else statement -->
+        </div>
+
         <!--THIS IS BOOTSTRAP JAVASRIPT PART START-->
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>   
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    
     </body>
 </html>
