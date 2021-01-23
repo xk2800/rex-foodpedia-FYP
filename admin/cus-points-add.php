fb3a7632befbbd25f134907c5291514976352818
eltonwcm00 <eltonwcm00@gmail.com>
2021-01-24 01:32:56 +0800

    cus points add (2.5/3)

--- admin/cus-points-add.php
+++ admin/cus-points-add.php
@@ -16,6 +16,32 @@
 
         <!--FONTS.CSS-->
         <link rel="stylesheet" href="css/fonts.css">
+
+        <style>
+            
+            #title-cus-add {
+                font-size: 1.5em;
+                font-weight: 300;
+            }
+
+            ::placeholder {
+                font-style: italic;
+                font-size: 0.9em;
+            }
+            
+            #small-cus-add  {
+                margin-left: 10px;
+                font-weight: lighter;
+                font-size: 0.7em;
+                font-style: italic;
+                color: #9c9c9c;   
+            }
+
+            #point-cus-add {
+                margin-top: 13px;
+            }
+
+        </style>
     </head>
     
     <body style="background-color: #4C5C68;">     
@@ -26,21 +52,56 @@
 
         <div class="container">
             <form>
+                
+                <!-- will need to adjust it to be at the center of the screen after backend is setup -->
                 <div class="row mb-4 mt-5">
                     <div class="form-group col-md-9">
-                        <input id="exampleFormControlInput5" type="email" placeholder="Insert the user email" class="form-control form-control-underlined">
+                        <input id="exampleFormControlInput5" type="email" placeholder="Kindly insert the user email that would like to add points" class="form-control form-control-underlined">
                     </div>
                     <div class="form-group col-md-3">
                         <button type="submit" class="btn btn-primary rounded-pill btn-block shadow-sm">Search</button>
                     </div>
-                </div> 
+                </div>
+                <!-- end statement -->
             </form>
 
             <!-- if-else statement will go here, state=true -> statement block will be executed || state=false -> alert box display the error -->
                 <div class="card">
-                    <h5 class="card-header">Add Points</h5>
+                    <h5 class="card-header" id="title-cus-add">Add Points</h5>
                     <div class="card-body">
-                        <a href="#" class="btn btn-primary btn-sm float-right">Save</a>
+                    
+                    <form>
+                        <div class="form-group row">
+                            <label for="point-cus-add" class="col-sm-2 col-form-label"><span style="color:red">*&nbsp;</span>Points<br/>
+                                <span id="small-cus-add">Set the number of points</span>
+                            </label>
+                            <div class="col-sm-10">
+                                <input type="number" class="form-control" id="point-cus-add" required>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="point-cus-add" class="col-sm-2 col-form-label"><span style="color:red">*&nbsp;</span>Validity<br/>
+                                <span id="small-cus-add">Set the validity of points (days)</span>
+                            </label>
+                            <div class="col-sm-10">
+                                <input type="number" class="form-control" id="point-cus-add" required>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="point-cus-add" class="col-sm-2 col-form-label"><span style="color:red">*&nbsp;</span>Status<br/>
+                                <span id="small-cus-add">Choose status for points</span>
+                            </label>
+                            <div class="col-sm-10">
+                                <select class="form-control" id="exampleFormControlSelect1">
+                                    <option>Available</option>
+                                    <option>Unavailable</option>
+                                </select>
+                            </div>
+                        </div>
+                        <a href="#" class="btn btn-primary" style="margin: 0.5em 0em 2em 11.5em;">Save</a>
+                    </form
+                    
+                        
                     </div>    
                 </div>
             <!-- end of if-else statement -->
