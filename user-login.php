2aad1ad9d053c7b8fc9e622c9f030ad4945035a9
Xavier K <xavierkhew00@gmail.com>
2021-01-31 03:25:49 +0800

    test.php

--- user-login.php
+++ user-login.php
@@ -1,12 +1,22 @@
- <?php
- ob_start();
-require 'db_connection.php';
+<?php
+session_start();
+session_regenerate_id(true);
 
-/*
-if(isset($_SESSION['login_id'])){
-    header('Location: user-login');
+//set default time zone
+date_default_timezone_set("Asia/Kuala_Lumpur");
+
+// change the information according to your database
+$db_connection = mysqli_connect("13.212.90.225", "database", "DZUUuox3oDVC3s3A","rex-foodipedia");
+// CHECK DATABASE CONNECTION
+if(mysqli_connect_errno()){
+    echo "Connection Failed".mysqli_connect_error();
     exit;
-}*/
+}
+
+if(isset($_SESSION['login_id'])){
+    header('Location: home.php');
+    exit;
+}
 
 require 'google/vendor/autoload.php';
 
@@ -18,7 +28,7 @@ $client->setClientId('981967059646-u56d1fku9i52fb53rb9o7t6deav37ddq.apps.googleu
 // Enter your Client Secrect
 $client->setClientSecret('_TTBH-saxhTlvSHZVt-COrHw');
 // Enter the Redirect URL
-$client->setRedirectUri('https://rex-foodipedia-fyp.herokuapp.com/user-login.php');
+$client->setRedirectUri('https://test-google-api-xk.herokuapp.com/add.php');
 
 // Adding those scopes which we want to get (email & profile Information)
 $client->addScope("email");
@@ -42,25 +52,24 @@ if(isset($_GET['code'])):
         $full_name = mysqli_real_escape_string($db_connection, trim($google_account_info->name));
         $email = mysqli_real_escape_string($db_connection, $google_account_info->email);
         $profile_pic = mysqli_real_escape_string($db_connection, $google_account_info->picture);
-        $verified = "1";
 
         // checking user already exists or not
         $get_user = mysqli_query($db_connection, "SELECT `google_id` FROM `user_acc` WHERE `google_id`='$id'");
         if(mysqli_num_rows($get_user) > 0){
 
             $_SESSION['login_id'] = $id; 
-            header('Location: menu.php');
+            header('Location: db_connection.php');
             exit;
 
         }
         else{
 
-            /// if user not exists we will insert the user
-            $insert = mysqli_query($db_connection, "INSERT INTO `user_acc`(`google_id`,`name`,`email`,`profile_image`, `verified`) VALUES('$id','$full_name','$email','$profile_pic', '$verified')");
+            // if user not exists we will insert the user
+            $insert = mysqli_query($db_connection, "INSERT INTO `user_acc`(`google_id`,`name`,`email`,`profile_image`) VALUES('$id','$full_name','$email','$profile_pic')");
 
             if($insert){
                 $_SESSION['login_id'] = $id; 
-                header('Location: menu.php');
+                header('Location: db_connection.php');
                 exit;
             }
             else{
@@ -71,11 +80,11 @@ if(isset($_GET['code'])):
 
     }
     else{
-        header('Location: user-login');
+        header('Location: login.php');
         exit;
     }
-else:
-
+    
+else: 
     // Google Login Url = $client->createAuthUrl(); 
 ?>
 
