7ac3c1a99a16d2f52ce1a3d959c0b9a3e98d94cd
Xavier K <xavierkhew00@gmail.com>
2021-01-31 04:05:36 +0800

    gogle test

--- test.php
+++ test.php
@@ -1,17 +1,5 @@
 <?php
-session_start();
-session_regenerate_id(true);
-
-//set default time zone
-date_default_timezone_set("Asia/Kuala_Lumpur");
-
-// change the information according to your database
-$db_connection = mysqli_connect("13.212.90.225", "database", "DZUUuox3oDVC3s3A","rex-foodipedia");
-// CHECK DATABASE CONNECTION
-if(mysqli_connect_errno()){
-    echo "Connection Failed".mysqli_connect_error();
-    exit;
-}
+require 'db_connection.php';
 
 if(isset($_SESSION['login_id'])){
     header('Location: home.php');
@@ -28,7 +16,7 @@ $client->setClientId('981967059646-u56d1fku9i52fb53rb9o7t6deav37ddq.apps.googleu
 // Enter your Client Secrect
 $client->setClientSecret('_TTBH-saxhTlvSHZVt-COrHw');
 // Enter the Redirect URL
-$client->setRedirectUri('https://rex-foodipedia-fyp.herokuapp.com/test.php');
+$client->setRedirectUri('https://rex-foodipedia-fyp.herokuapp.com/user-login.php');
 
 // Adding those scopes which we want to get (email & profile Information)
 $client->addScope("email");
@@ -58,7 +46,7 @@ if(isset($_GET['code'])):
         if(mysqli_num_rows($get_user) > 0){
 
             $_SESSION['login_id'] = $id; 
-            header('Location: index.php');
+            header('Location: db_connection.php');
             exit;
 
         }
@@ -69,7 +57,7 @@ if(isset($_GET['code'])):
 
             if($insert){
                 $_SESSION['login_id'] = $id; 
-                header('Location: index.php');
+                header('Location: db_connection.php');
                 exit;
             }
             else{
@@ -87,5 +75,7 @@ if(isset($_GET['code'])):
 else: 
     // Google Login Url = $client->createAuthUrl(); 
 ?>
-<a class="login-btn" href="<?php echo $client->createAuthUrl(); ?>">Sign in with Google</a>
+
+    <a class="login-btn" href="<?php echo $client->createAuthUrl(); ?>">Login-test</a>
+
 <?php endif; ?>
