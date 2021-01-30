5e4197809343bb1ee124236404ddc58f31b89eb7
Xavier K <xavierkhew00@gmail.com>
2021-01-31 00:31:54 +0800

    test more hai

--- user-login.php
+++ user-login.php
@@ -18,7 +18,7 @@ $client->setClientId('981967059646-u56d1fku9i52fb53rb9o7t6deav37ddq.apps.googleu
 // Enter your Client Secrect
 $client->setClientSecret('_TTBH-saxhTlvSHZVt-COrHw');
 // Enter the Redirect URL
-$client->setRedirectUri('https://rex-foodipedia-fyp.herokuapp.com/user-login');
+$client->setRedirectUri('https://rex-foodipedia-fyp.herokuapp.com/user-login.php');
 
 // Adding those scopes which we want to get (email & profile Information)
 $client->addScope("email");
@@ -49,7 +49,7 @@ if(isset($_GET['code'])){
         if(mysqli_num_rows($get_user) > 0){
 
             $_SESSION['login_id'] = $id; 
-            header('Location: menu');
+            header('Location: menu.php');
             exit;
 
         }
@@ -60,7 +60,7 @@ if(isset($_GET['code'])){
 
             if($insert){
                 $_SESSION['login_id'] = $id; 
-                header('Location: menu');
+                header('Location: menu.php');
                 exit;
             }
             else{
