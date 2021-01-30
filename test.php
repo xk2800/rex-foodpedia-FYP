c0fcd644b8d1166ca0655bd8bde8dd2934383c78
Xavier K <xavierkhew00@gmail.com>
2021-01-31 03:47:23 +0800

    test

--- test.php
+++ test.php
@@ -1,11 +1,22 @@
 <?php
-ob_start();
-require 'db_connection.php';
-/*
+session_start();
+session_regenerate_id(true);
+
+//set default time zone
+date_default_timezone_set("Asia/Kuala_Lumpur");
+
+// change the information according to your database
+$db_connection = mysqli_connect("13.212.90.225", "database", "DZUUuox3oDVC3s3A","rex-foodipedia");
+// CHECK DATABASE CONNECTION
+if(mysqli_connect_errno()){
+    echo "Connection Failed".mysqli_connect_error();
+    exit;
+}
+
 if(isset($_SESSION['login_id'])){
     header('Location: home.php');
     exit;
-}*/
+}
 
 require 'google/vendor/autoload.php';
 
@@ -13,9 +24,9 @@ require 'google/vendor/autoload.php';
 $client = new Google_Client();
 
 // Enter your Client ID
-$client->setClientId('742809127078-hqo1nv9dnofnpslrpantsj82lg06bbc9.apps.googleusercontent.com');
+$client->setClientId('981967059646-u56d1fku9i52fb53rb9o7t6deav37ddq.apps.googleusercontent.com');
 // Enter your Client Secrect
-$client->setClientSecret('BFRvg1hEBJtDO4hBc5Ugl1v_');
+$client->setClientSecret('_TTBH-saxhTlvSHZVt-COrHw');
 // Enter the Redirect URL
 $client->setRedirectUri('https://rex-foodipedia-fyp.herokuapp.com/test.php');
 
@@ -43,22 +54,22 @@ if(isset($_GET['code'])):
         $profile_pic = mysqli_real_escape_string($db_connection, $google_account_info->picture);
 
         // checking user already exists or not
-        $get_user = mysqli_query($db_connection, "SELECT `google_id` FROM `users` WHERE `google_id`='$id'");
+        $get_user = mysqli_query($db_connection, "SELECT `google_id` FROM `user_acc` WHERE `google_id`='$id'");
         if(mysqli_num_rows($get_user) > 0){
 
             $_SESSION['login_id'] = $id; 
-            header('Location: db_connection.php');
+            header('Location: index.php');
             exit;
 
         }
         else{
 
             // if user not exists we will insert the user
-            $insert = mysqli_query($db_connection, "INSERT INTO `users`(`google_id`,`name`,`email`,`profile_image`) VALUES('$id','$full_name','$email','$profile_pic')");
+            $insert = mysqli_query($db_connection, "INSERT INTO `user_acc`(`google_id`,`name`,`email`,`profile_image`) VALUES('$id','$full_name','$email','$profile_pic')");
 
             if($insert){
                 $_SESSION['login_id'] = $id; 
-                header('Location: db_connection.php');
+                header('Location: index.php');
                 exit;
             }
             else{
@@ -76,7 +87,5 @@ if(isset($_GET['code'])):
 else: 
     // Google Login Url = $client->createAuthUrl(); 
 ?>
-
-    <a class="login-btn" href="<?php echo $client->createAuthUrl(); ?>">Login-test</a>
-
+<a class="login-btn" href="<?php echo $client->createAuthUrl(); ?>">Sign in with Google</a>
 <?php endif; ?>
